34aa56d7a0ba1fced077641dff2fc5859a273352
sergio costa <sergio.costa@ufma.br>
2022-01-11 18:09:44 -0300

    Initial commit

--- interpret.c
+++ interpret.c
@@ -1,6 +1,14 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 #include "interpret.h"
 
 void interpret (const char *source) {
+    char op[10];
+    char arg[10];
 
+    sscanf (source, "%s%s", op, arg);
+    printf("operação: %s\n", op);
+    printf("argumento: %s\n",  arg);
 }
