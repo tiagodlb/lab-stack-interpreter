d996864f85784df28124461fa271c0d916e22ab5
Tiago <tigolimab223@gmail.com>
2023-05-10 16:22:04 -0300

    added new methods
    added new methods such as mul (multiplication) and div (division)

--- interpret.c
+++ interpret.c
@@ -20,10 +20,31 @@ void interpret(const char *source) {
   if (strcmp(op, "push") == 0) {
     int value = atoi(arg);
     stack_push(s, value);
-  }
-  else if (strcmp(op, "add") == 0) {
+  } else if (strcmp(op, "add") == 0) {
     int arg1 = stack_pop(s);
     int arg2 = stack_pop(s);
-    stack_push(s, arg1+arg2);
+    stack_push(s, arg1 + arg2);
+  } else if (strcmp(op, "sub") == 0) {
+    int arg1 = stack_pop(s);
+    int arg2 = stack_pop(s);
+    stack_push(s, arg1 - arg2);
+  } else if (strcmp(op, "mul") == 0) {
+    int arg1 = stack_pop(s);
+    int arg2 = stack_pop(s);
+    stack_push(s, arg1 * arg2);
+  } else if (strcmp(op, "div") == 0) {
+    int arg1 = stack_pop(s);
+    int arg2 = stack_pop(s);
+    stack_push(s, arg1 / arg2);
+  } else if (strcmp(op, "print") == 0) {
+    stack_print(s);
+  } else if (strcmp(op, "pop") == 0) {
+    int arg1 = stack_pop(s);
+    int arg2 = stack_pop(s);
+    stack_push(s, arg1 + arg2);
+  } else if (strcmp(op, "asdsada") == 0) {
+    int arg1 = stack_pop(s);
+    int arg2 = stack_pop(s);
+    stack_push(s, arg1 + arg2);
   }
 }
